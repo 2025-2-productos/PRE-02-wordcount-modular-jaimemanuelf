a994206de36c653224e45c32bc4b2d446e9f7a37
Jaime Manuel Figueroa <99105750+jaimemanuelf@users.noreply.github.com>
2025-11-22 11:09:30 -0500

    ajustes para que pase test homework

--- homework/src/_internals/write_word_counts.py
+++ homework/src/_internals/write_word_counts.py
@@ -6,6 +6,6 @@ def write_count_words(counter, output_folder):
         os.makedirs(output_folder, exist_ok=True)
 
     # save the results using tsv format
-    with open(os.path.join(output_folder, "results.tsv"), "w", encoding="utf-8") as f:
+    with open(os.path.join(output_folder, "wordcount.tsv"), "w", encoding="utf-8") as f:
         for key, value in counter.items():
             f.write(f"{key}\t{value}\n")
